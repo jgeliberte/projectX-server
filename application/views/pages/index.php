639961bd9ee7d1f8017b18463166bf447e6cfd1b
John Geliberte <geliberteofficials@gmail.com>
2017-04-08 21:48:59 +0800

    Added Login UI

---
+++ application/views/pages/index.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="css/login.css">
+<div class="container-fluid">
+	<div class="row centralize-top"></div>
+	<div class="row">
+		<div class="col-xs-4"></div>
+		<div class="col-xs-4">
+			<div class="panel panel-primary content-center">
+			    <div class="panel-heading">Login User!</div>
+			    <div class="panel-body">
+				    <form>
+					    <div class="form-group">
+					    	<label for="username">Username</label>
+				    		<input id="username" class="form-control" type="text" placeholder="E.g JuanDelaCruz">
+					    	<label for="password">Password</label>
+				    		<input id="password" class="form-control" type="password" placeholder="************">
+				    		<a href="">Don't have a account?</a>
+				    		<input type="button" class="btn btn-primary form-control" value="Confirm">
+				    	</div>
+				    </form>
+			    </div>
+			</div>
+		</div>
+		<div class="col-xs-4 centralize-bottom"></div>
+	</div>
+	<div class="row"></div>
+</div>
---
+++ application/views/template/footer.php
@@ -0,0 +1,5 @@
+</body>
+<script src="js/jquery-3.2.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script scr="datatable.js"></script>
+</html>
