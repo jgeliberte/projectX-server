aa14dac1770a77231bb4473eb8cb3c47ba4f5c06
John Geliberte <geliberteofficials@gmail.com>
2017-04-09 23:17:45 +0800

    Typo error

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,5 +1,4 @@
 </body>
 <link rel="stylesheet" href="../../css/third-party/bootstrap.min.css">
 <link rel="stylesheet" href="../../css/third-party/datatable.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/styles/metro/notify-metro.css">
 </html>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -4,10 +4,8 @@
 	<meta charset="UTF-8">
 	<script type="text/javascript" src="../../js/third-party/jquery-3.2.0.min.js"></script>
 	<script type="text/javascript" src="../../js/third-party/bootstrap.min.js"></script>
-	<script type="text/javascript" src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.js"></script>
-	<!-- <script type="text/javascript" scr="js/third-party/datatable.js"></script>
-	<script type="text/javascript" scr="js/third-party/notify.min.js"></script> -->
+	<script type="text/javascript" src="../../js/third-party/datatable.js"></script>
+	<script type="text/javascript" src="../../js/third-party/notify.min.js"></script>
 
 	<title>DMS</title>
 </head>
