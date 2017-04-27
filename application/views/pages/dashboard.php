4093bbc730faa8ac60b9dbdbdfa80639896af86b
lenovo <lenovo>
2017-04-28 03:59:54 +0800

    changed the nav.php footer and header.php

---
+++ application/views/pages/dashboard.php
@@ -0,0 +1,15 @@
+<div style="display: flex; width: 100%; justify-content: center;">
+<div class='square-box'>
+    <div class='square-content user'><span class="textMenu">Patient</span></div>
+</div>
+<div class='square-box'>
+    <div class='square-content dental'><span class="textMenu">Dental</span>
+    <span></span></div>
+</div>
+<div class='square-box'>
+    <div class='square-content logs'><span class="textMenu">Logs</span></div>
+</div>
+<div class='square-box'>
+    <div class='square-content settings'><span class="textMenu">Settings</span></div>
+</div>
+</div>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+
 	<script type="text/javascript" src="../../js/third-party/jquery-3.2.0.min.js"></script>
 	<script type="text/javascript" src="../../js/third-party/bootstrap.min.js"></script>
 	<script type="text/javascript" src="../../js/third-party/datatable.js"></script>
