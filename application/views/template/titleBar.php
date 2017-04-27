1bbb78f86fbb55a86df80f87f045d75ca29f1921
lenovo <lenovo>
2017-04-28 04:03:34 +0800

    added titleBar

---
+++ application/views/template/titleBar.php
@@ -0,0 +1,20 @@
+<div class="panel panel-primary content-center">
+  <nav class="headerBar panel-heading navbar-">
+    <div class="container-fluid">
+      <div class="navbar-header">
+      <span class="navbar-brand" href="#">Dental Management System</span>
+      </div>
+      <div class="pull-right">
+        <div class="btn-group headerVerticalCenter">
+          <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+            User
+            <span class="caret"></span>
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+            <li><a href="#">Log out</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</div>
