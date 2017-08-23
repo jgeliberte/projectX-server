e62c760243916400efcc351a9528f3b54a25a385
jgeliberte <gelibertejohn@gmail.com>
2017-08-23 10:49:29 +0800

    TEST

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
-$db['default']['database'] = 'project_db';
+$db['default']['password'] = 'senslope';
+$db['default']['database'] = 'project_dms';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
